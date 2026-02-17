7d8fa4a6b149eeee3a78bc6aa579503cae605e2e
Elias Franco Duarte <eliasdfranco@hotmail.com>
2026-02-17 02:12:07 -0800

    feat(datasource): implement MoviedbDatasource for upcoming movies

--- lib/infraestructure/datasources/moviedb_datasource.dart
+++ lib/infraestructure/datasources/moviedb_datasource.dart
@@ -34,4 +34,10 @@ class MoviesdbDatasource extends MoviesDatasource {
     final response = await dio.get('/movie/popular');
     return _jsonToMovies(response.data);
   }
+
+  @override
+  Future<List<Movie>> getUpcoming({int page = 1}) async {
+    final response = await dio.get('/movie/upcoming');
+    return _jsonToMovies(response.data);
+  }
 }
